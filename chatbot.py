5de4d983f9b6aa29bcc71c9b551e52ff5852a317
csauyong <auyongchunsang@gmail.com>
2023-06-07 11:02:10 -0400

    Added yes and no button

--- chatbot.py
+++ chatbot.py
@@ -7,6 +7,10 @@ from dotenv import load_dotenv
 # A simple in-memory data structure to store stress keyword counts and chat history
 # In a production environment, consider using a more persistent storage, like a database
 stress_counts = {}
+
+# Dictionary of lists of dictionaries
+# Each list is a chat record of a user
+# Each inner dictionary contains the role and content of message
 conversation_history = {}
 
 STRESS_KEYWORDS = [
@@ -20,8 +24,10 @@ STRESS_KEYWORDS = [
     'unmotivated', 'procrastinating', 'overthinking', 'doubtful', 'uncertain'
 ]
 STRESS_KEYWORD_THRESHOLD = 3
-HIGH_STRESS_MESSAGE = "I understand your situation. Being a caregiver could be exhausting and may affect you mentally. Don't forget your well-being before taking care of others. In addition to seeking support from family and friends, there are also support groups in Hong Kong for caregivers of people with dementia. Would you like me to refer you to some of these groups?"
+HIGH_STRESS_MESSAGE = "\n\nI understand your situation. Being a caregiver could be exhausting and may affect you mentally. Don't forget your well-being before taking care of others. In addition to seeking support from family and friends, there are also support groups in Hong Kong for caregivers of people with dementia. Would you like me to refer you to some of these groups?"
 
+YES_TO_RESOURCE = "Great. Here are a few support groups in Hong Kong that you may find helpful:\nThe Hong Kong Alzheimer's Disease Association: http://www.hkalzheimers.org/\nThe Carers Support Group: https://www.carers.org.hk/\nThe Mental Health Association of Hong Kong: https://www.mhahk.org.hk/\nI hope these resources can provide you with the support and guidance you need during this challenging time. Please don't hesitate to reach out if you need further assistance."
+NO_TO_RESOURCE = "It's ok. I will always be here for you. "
 # Load API keys from the .env file
 load_dotenv("keys.env")
 TELEGRAM_API_KEY = os.getenv("TELEGRAM_API_KEY")
@@ -35,7 +41,7 @@ bot = telebot.TeleBot(TELEGRAM_API_KEY)
 def send_welcome(message):
     bot.reply_to(message, "Hello, how are you doing?")
     stress_counts[message.chat.id] = 0
-    conversation_history[message.chat.id] = ""
+    conversation_history[message.chat.id] = [{"role": "system", "content": "You are a friendly assitant who talks like texting. Keep the message short. Use emoji when suitable. The user is a caregiver in struggle."}]
 
 def analyze_stress_level(user_message, user_id, stress_counts):
     stress_keyword_count = sum([keyword in user_message.lower() for keyword in STRESS_KEYWORDS])
@@ -43,19 +49,18 @@ def analyze_stress_level(user_message, user_id, stress_counts):
     
 def generate_gpt_response(user_message, user_id, stress_counts, conversation_history):
     analyze_stress_level(user_message, user_id, stress_counts)
-    conversation_history[user_id] += f"User: {user_message}\nAI:"
-    # response = openai.Completion.create(
-    #     engine="text-davinci-002",
-    #     prompt=f"You are a friendly and casual AI who talks like a human friend.\n{conversation_history}",
-    #     temperature=0.8,
-    #     max_tokens=50,
-    #     top_p=1,
-    #     frequency_penalty=0,
-    #     presence_penalty=0,
-    # )
-    # response_text = response.choices[0].text.strip()
-    response_text = f"You are user {user_id}. You have mentioned {stress_counts[user_id]} keywords."
-    conversation_history[user_id] += f" {response_text}\n"
+    conversation_history[user_id].append({"role": "user", "content": user_message}) 
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=conversation_history[user_id],
+        temperature=0.8,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+    )
+    response_text = response.choices[0].message.content.strip()
+    conversation_history[user_id].append({"role": "assistant", "content": response_text})
     print(conversation_history)
 
     # Check if the stress keyword count has reached the threshold
@@ -85,9 +90,10 @@ def query_handler(call):
     chat_id = call.message.chat.id
 
     if answer == 'yes':
-        bot.send_message(chat_id, "Great. Here are a few support groups in Hong Kong that you may find helpful:\nThe Hong Kong Alzheimer's Disease Association: http://www.hkalzheimers.org/\nThe Carers Support Group: https://www.carers.org.hk/\nThe Mental Health Association of Hong Kong: https://www.mhahk.org.hk/\nI hope these resources can provide you with the support and guidance you need during this challenging time. Please don't hesitate to reach out if you need further assistance.")
+        bot_response = YES_TO_RESOURCE
     else:
-        bot.send_message(chat_id, 'It alright. I am always here for you.')
+        bot_response = NO_TO_RESOURCE
+    bot.send_message(chat_id, bot_response)
 
 # Start the bot's polling loop
 bot.polling()
